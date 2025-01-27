418a1125013510140d3160bef5febc45f97263f5
Tushar9582 <tushargoswami6263@gmail.com>
2025-01-27 16:56:01 +0530

    code pushed

--- src/Pages/Call.jsx
+++ src/Pages/Call.jsx
@@ -2,9 +2,15 @@ import "./call.css";
 
 const Call = () => {
   return (
-    <div className="container-fluid bg-call-to-action py-5" style={{marginTop:'50px',marginBottom:'50px'}}>
+    <div className="container-fluid bg-call-to-action py-5" style={{ marginTop: '50px', marginBottom: '50px' }}>
       <div className="container py-5">
-        <div className="row g-0 justify-content-end custom-row">
+        <div className="row g-0 justify-content-center custom-row">
+          <div className="col-12 text-center">
+            <div className="overlay-container">
+              <h1 className="project">Do you have any project?</h1>
+              <p className="para">Waterproofing your home office is exciting, but choosing the right products and servicemen while ensuring your safety can be a huge task. That’s where we come in. Our aim is to make home waterproofing a safe and great experience with IWC services. Reimagine your home office as a huge canvas—start your journey with IWC services and let us guide you through all the questions you might have.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,81 +1,4 @@
-// import React from 'react';
-// import './Footer.css';
 
-// const Footer = () => {
-//   return (
-//     <>
-//       <div className="container-fluid  bg-footer text-light py-5 content"  >
-//         <div className="container py-5">
-//           <div className="row g-5">
-//             <div className="col-lg-3 col-md-6">
-//               <h4 className="text" style={{color:'blue'}}>Get In Touch</h4>
-//               <hr className="w-25 text-secondary mb-4" style={{ opacity: 1 }} />
-//               <p className="mb-4" style={{color:' #04334f'}}>No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed dolor</p>
-//               <p className="mb-2"style={{color:' #04334f'}}><i className="fa fa-map-marker-alt text-info me-3"></i>123 Street, New York, USA</p>
-//               <p className="mb-2"style={{color:' #04334f'}}><i className="fa fa-envelope text-info me-3"></i>info@example.com</p>
-//               <p className="mb-0"style={{color:' #04334f'}}><i className="fa fa-phone-alt text-info me-3"></i>+012 345 67890</p>
-//             </div>
-//             <div className="col-lg-3 col-md-6">
-//               <h4 className="text" style={{color:'blue'}}>Our Services</h4>
-//               <hr className="w-25 text-secondary mb-4" style={{ opacity: 1 }} />
-//               <div className="d-flex flex-column justify-content-start"style={{color:' #04334f'}}>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Regular Painting</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Wall Painting</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Floor Coating</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Graffiti Removal</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Mildew Removal</a>
-//                 <a className="" href="#"><i className="fa fa-angle-right me-2"></i>Window Washing</a>
-//               </div>
-//             </div>
-//             <div className="col-lg-3 col-md-6">
-//               <h4 className="text" style={{color:'blue'}}>Quick Links</h4>
-//               <hr className="w-25 text-secondary mb-4" style={{ opacity: 1 }} />
-//               <div className="d-flex flex-column justify-content-start" style={{color:' #04334f'}}>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Home</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>About Us</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Our Services</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Meet The Team</a>
-//                 <a className=" mb-2" href="#"><i className="fa fa-angle-right me-2"></i>Latest Blog</a>
-//                 <a className="" href="#"><i className="fa fa-angle-right me-2"></i>Contact Us</a>
-//               </div>
-//             </div>
-//             <div className="col-lg-3 col-md-6">
-//               <h4 className="text" style={{color:'blue'}}>Newsletter</h4>
-//               <hr className="w-25  mb-4" style={{ opacity: 1 }} />
-//               <form action="">
-//                 <div className="input-group"style={{color:' #04334f'}}>
-//                   <input type="text" className="form-control p-3 border-0" placeholder="Your Email" />
-//                   <button className="btn btn-info" style={{background:'blue'}}>Sign Up</button>
-//                 </div>
-//               </form>
-//               <h6 className="text mt-4 mb-3">Follow Us</h6>
-//               <div className="d-flex"  >
-//                 <a className="btn btn-lg  btn-lg-square rounded-circle me-2" href="#"><i className="fab fa-twitter"  style={{color:'white'}}></i></a>
-//                 <a className="btn btn-lg  btn-lg-square rounded-circle me-2" href="#"><i className="fab fa-facebook-f"style={{color:'white'}}></i></a>
-//                 <a className="btn btn-lg btn-lg-square rounded-circle me-2" href="#"><i className="fab fa-linkedin-in"style={{color:'white'}}></i></a>
-//                 <a className="btn btn-lg  btn-lg-square rounded-circle" href="#"><i className="fab fa-instagram"style={{color:'white'}}></i></a>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="container-fluid  text-white py-1"  style={{background:'#0b5079'}}>
-//         <div className="container" >
-//           <div className="row g-0">
-//             <div className="col-md-6 text-center text-md-start">
-//               <p className="mb-md-0">Copyright &copy; <a className="text-white fw-bold" href="#">Your Site Name</a>. All Rights Reserved.</p>
-//             </div>
-//             <div className="col-md-6 text-center text-md-end">
-//               <p className="mb-0">Designed by <a href="https://pawartechnologyservices.com/" target="_blank" className="footer-link" style={{ color: 'or' }}>PTS</a></p>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Footer;
 import React from 'react';
 import './Footer.css';
 
@@ -97,7 +20,7 @@ const Footer = () => {
               </p>
               <p className="footer-item">
               <i className="fa fa-envelope" style={{ color: '#0b5079' }} me-3></i>
-                info@example.com
+                   info@example.com
               </p>
               <p className="footer-item">
               <i className="fa fa-phone-alt" style={{ color: '#0b5079' }} me-3></i>
