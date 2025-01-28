a40f650c3ade534812079e2e4086648f412a9547
pratik7020 <pratikfutane19@gmail.com>
2025-01-28 11:17:31 +0530

    done

--- src/Pages/Call.jsx
+++ src/Pages/Call.jsx
@@ -2,7 +2,7 @@ import "./call.css";
 
 const Call = () => {
   return (
-    <div className="container-fluid bg-call-to-action py-5" style={{ marginTop: '50px', marginBottom: '50px' }}>
+    <div className="container-fluid bg-call-to-action ">
       <div className="container py-5">
         <div className="row g-0 justify-content-center custom-row">
           <div className="col-12 text-center">
